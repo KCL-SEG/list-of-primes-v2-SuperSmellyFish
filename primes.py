7ce830c57afd4686e321076e3bc495b65ef5563c
Kai Jones <kai.jones@kcl.ac.uk>
2023-11-27 15:06:09 +0000

    it might work

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer.")
+
+    prime_list = []
+    current_number = 2
+
+    while len(prime_list) < number_of_primes:
+        is_prime = all(current_number % prime != 0 for prime in prime_list)
+        if is_prime:
+            prime_list.append(current_number)
+        current_number += 1
+
+    return prime_list
